e2caa6beb25eb35126d16f3a48a929098e3de9b4
Fahimrahman582 <113621101+Fahimrahman582@users.noreply.github.com>
2023-06-18 00:06:21 +0600

    main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import random
 def generate_numbers(first_digits):
     numbers = []
     for first_digit in first_digits:
-        for _ in range(20):
+        for _ in range(10):
             number = str(first_digit)
-            for _ in range(5):
+            for _ in range(7):
                 number += str(random.randint(0, 9))
             numbers.append(number)
     
@@ -17,3 +17,4 @@ def generate_numbers(first_digits):
 result = generate_numbers([4, 5])
 print(result)
 
+ 
