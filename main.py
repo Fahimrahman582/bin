a5e3e9c7ddb729446c81977ff442745bdb7a2dd2
Fahimrahman582 <113621101+Fahimrahman582@users.noreply.github.com>
2023-06-17 23:20:22 +0600

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def generate_numbers(first_digits):
             for _ in range(5):
                 number += str(random.randint(0, 9))
             numbers.append(number)
-    return numbers
+    
+    # Concatenate numbers into a string
+    numbers_string = "\n".join(numbers)
+    return numbers_string
 
 # Call the function with a list of first digits
 result = generate_numbers([4, 5])
