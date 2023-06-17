bd241b39a64fa8b1c5e335b752e0407162e1c86b
Fahimrahman582 <113621101+Fahimrahman582@users.noreply.github.com>
2023-06-17 22:11:00 +0600

    main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+def generate_numbers(first_digits):
+    numbers = []
+    for first_digit in first_digits:
+        for _ in range(20):
+            number = str(first_digit)
+            for _ in range(5):
+                number += str(random.randint(0, 9))
+            numbers.append(number)
+    return numbers
+
+# Call the function with a list of first digits
+result = generate_numbers([4, 5])
+print(result)
+
